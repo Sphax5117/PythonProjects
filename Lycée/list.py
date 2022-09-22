9922d039af9e7cddb473e52c16066fbb81d9fbda
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2022-09-22 09:30:47 +0200

    listes

---
+++ Lycée/list.py
@@ -0,0 +1,12 @@
+# Créé par t.hausmann, le 22/09/2022 en Python 3.7
+L1 = [5**0, 5**1, 5**2, 5**3, 5**4, 5**5]
+L2 = [1**2, 2**2, 3**2, 4**2, 5**2]
+L3 = [0.15*1, 0.15*2, 0.15*3, 0.15*4, 0.15*5, 0.15*6]
+L4 = [0.25, 0.5, 0.75, 1.0, 1.25, 1.50, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0]
+L5 = [0.1 ** 3, 0.2**3, 0.3**3, 0.4**3 , 0.5**3, 0.6**3, 0.7, 0.8**3, 0.9**3, 1.0**3, 1.1**3, 1.2*3, 1.3**3, 1.4**3, 1.5**3, 1.6**3, 1.7**3, 1.8**3, 1.9**3, 2.0**3]
+#On marque 1 in L1 dans la console pour vérifier si 1 est dans L1
+#L3[2] = 7 remplace la veleur a l'emplacement 2 (le troisième dans la liste donc) par 7
+#del L3[2] supprime la valeur à l'emplacement 2
+#L3.append(6) rajoute 6 à la fin de la liste
+#L3 = [1] + L3 ajoute les valeurs de L3 à la fin d'une liste contenant 1
+#L3 = L3 + [2.7] ajoute 2.7 a la fin de la liste L3
