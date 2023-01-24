3a394878e0d3efcbbaf1da405a72c90f0bcd02f3
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-01-24 13:05:24 +0100

    finshed it

--- Lycée/MOLKI.py
+++ Lycée/MOLKI.py
@@ -1,27 +1,29 @@
 # a. Dictionnaire
 from copy import deepcopy
-valeur =  {"joueur_1":0, "joueur_2":0, "joueur_3":0}
+valeur =  {"joueur_1":[0,0], "joueur_2":[0,0], "joueur_3":[0,0]}
 
 def MlK(valeur):
     win = False
-    j = None
-    nbr0 = {"joueur_1":0, "joueur_2":0, "joueur_3":0}       # dictionnaire comptant les 0 de chaque joueur
+    j = None   # dictionnaire comptant les 0 de chaque joueur
     while win == False:
         for i in valeur:
+            print(valeur[i][1])
             inpt = int(input(f"{i} :"))
-            if inpt <= 12:                                  # commence le comptage de pts si score inferieur a 12
-                valeur[i] += inpt
-                if valeur[i] == 50:                         # verifie si le score = à 50 et fait gagner la personne qui a win
+            if inpt <= 12:
+                if inpt != 0:                                  # commence le comptage de pts si score inferieur a 12
+                    valeur[i][0] += inpt
+                    valeur[i][1] = 0
+                if valeur[i][0] == 50:                         # verifie si le score = à 50 et fait gagner la personne qui a win
                     print(i, "a gagné")
                     return valeur
-                elif valeur[i] > 50:                        # vérifie si le score est supérieur à 50 puis le remet a 25
+                elif valeur[i][0] > 50:                        # vérifie si le score est supérieur à 50 puis le remet a 25
                     print("Score dépasse 50 : passe a 25")
-                    valeur[i] = 25
+                    valeur[i][0] = 25
                     print(valeur)
                 elif inpt == 0:                             # si le score d'un joueur est 0 ça ajoute un au compteur de zero
-                    nbr0[i] += 1
-                    print(nbr0[i])
-                    if nbr0[i] == 3:
+                    valeur[i][1] += 1
+                    print(valeur[i][1])
+                    if valeur[i][1] == 3:
                         j = i                        # si le joueur a 3 zeros il est éliminé
                         print(i, "a perdu")                        
                         break
