0ca024f50afaa0e3b4b72276e5a779826e2de329
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-02-09 12:01:12 +0100

    Update révision_fonctions.py

--- Lycée/révision_fonctions.py
+++ Lycée/révision_fonctions.py
@@ -14,26 +14,25 @@ def max(liste):
             max = value
     return max
 
+def tri_selection(liste):
+    for i in range(len(liste)):
+        mini = i
+        for j in range(len(liste)):
+            if liste[i] < liste[j]:
+                liste[i], liste[j] = liste[j], liste[i]
+    print(liste)
+
+    return liste
+
 def tri_insertion(liste):
     for i in range(len(liste)):
         while i > 0 and liste[i] < liste[i - 1]:
             liste[i], liste[i - 1] = liste[i - 1], liste[i]
-            i = i -1
+            i = i -1 
     print(liste)
-    return liste
-
-def tri_selection(liste):
-    for i in range(len(liste)):
-        mini = i
-        for j in range(i + 1, len(liste)):
-            if liste[j] < liste[mini]:
-                liste[j], liste[mini] = liste[mini], liste[j]
-
-    return liste
 
 
-
-tri_insertion(to_sort)
 tri_selection(to_sort)
+tri_insertion(to_sort)
 min(to_sort)
 max(to_sort)
