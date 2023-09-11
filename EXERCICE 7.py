90e45da2c777b90227eeffe506e536ef0675d1e9
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-09-11 15:10:25 +0200

    Add files via upload

--- EXERCICE 7.py
+++ EXERCICE 7.py
@@ -1,3 +1,14 @@
 # Créé par t.hausmann, le 11/09/2023 en Python 3.7
+list_data = ["clé1", "valuer", "clé2", "valeur"]
 
-def list_to_dico():
+def list_to_dict(list_data):
+  dict_data = {}
+  for index in range(0, len(list_data), 2):
+    key = list_data[index]
+    value = list_data[index + 1]
+    dict_data[key] = value
+
+  print(dict_data)
+  return dict_data
+
+list_to_dict(list_data)
