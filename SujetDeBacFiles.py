f002b2c5d212a845a54a2ea453c243eda331a2ae
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2023-09-18 14:48:12 +0200

    Add files via upload

--- SujetDeBacFiles.py
+++ SujetDeBacFiles.py
@@ -30,10 +30,27 @@ def variation(F):
     else:
         tab = [0 for k in range (len(F) -1)]
         element1 = defile(F)
-        for i in range(len(F) -1):
+        for i in range(len(F)):
             element2= defile(F)
-            print(element1, element2)
-            tab[i] = int(element1) - int(element2)
 
+            tab[i] = int(element2) - int(element1)
+            element1 = element2
+            print(element1, element2)
+    print(tab)
     return tab
 
+
+def nombre_baisse(f):
+    maxDown = 1000
+    dayCount = 0
+    for i in range(len(f) -1):
+        if f[i] > f[i+1]:
+            dayCount += 1
+    for i in range(len(f)):
+        if f[i] < maxDown:
+            maxDown = f[i]
+
+    print(dayCount, maxDown)
+    return dayCount
+
+nombre_baisse([1, -4, 2, -1, -2])
